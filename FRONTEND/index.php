672dce85f6c91637ed6ff7ba49f34075ab01e27f
juanaco56 <juan.fortnite56@gmail.com>
2025-11-27 21:05:17 +0100

    cambios_

--- FRONTEND/index.php
+++ FRONTEND/index.php
@@ -1,5 +1,5 @@
 <?php 
-    $apiUrl = getenv('API_URL');
+    $apiUrl = getenv('https://api-rest-java-pets.onrender.com');
     $data = @file_get_contents($url);
     $pets = json_decode($data, true) ?? [];
 ?>
